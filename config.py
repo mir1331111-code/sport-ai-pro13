f19c0cc8962e1bcf4c6dd6b272294df3b1b577b3
mir1331111-code <mir1331111@gmail.com>
2026-09-20 00:41:14 +0500

    Update config.py

--- config.py
+++ config.py
@@ -28,9 +28,9 @@ CACHE_TTL = {
 }
 
 LLM_PROVIDERS = {
-        "Groq (бесплатно, быстро)": {
+            "Groq (бесплатно, быстро)": {
         "base": "https://api.groq.com/openai/v1",
-        "model": "llama-3.1-8b-instant",
+        "model": "openai/gpt-oss-120b",
         "key_url": "https://console.groq.com/keys",
     },
             "Gemini (Google, бесплатно)": {
